2ae6e3fdcfc151e51fba7f88e3439c9d140b5add
Gungun <gungun.21b1531195@abes.ac.in>
2025-04-22 17:54:03 +0530

    yes added

--- k.py
+++ k.py
@@ -28,4 +28,4 @@ def get_weather():
         return jsonify({"error": "City not found or API error"}), response.status_code
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=5000)
